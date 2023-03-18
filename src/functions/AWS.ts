a92aa682413354a07aad0011cf275c17f86570bb
Geralt-Of-Rivia-Witcher <singhsiddhantkumar@gmail.com>
2023-03-19 00:19:15 +0530

    feat: :sparkles: upload to s3

---
+++ src/functions/AWS.ts
@@ -0,0 +1,17 @@
+import AWS from "aws-sdk";
+
+AWS.config.update({
+  accessKeyId: process.env.AWS_ACCESS_KEY_ID,
+  secretAccessKey: process.env.AWS_SECRET_ACCESS_KEY,
+  region: process.env.AWS_REGION,
+});
+
+const s3 = new AWS.S3();
+
+export const uploadToS3 = async (data, key) => {
+  const file = await s3.upload({
+    Bucket: process.env.AWS_BUCKET_NAME,
+    Key: key,
+    Body: data,
+  });
+};
