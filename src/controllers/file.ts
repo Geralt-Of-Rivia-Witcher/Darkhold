10030992f74c2f889f63d3146dba748d2138dffc
Geralt-Of-Rivia-Witcher <singhsiddhantkumar@gmail.com>
2023-03-19 17:27:04 +0530

    refactor: :recycle: Minor refactoring

--- src/controllers/file.ts
+++ src/controllers/file.ts
@@ -11,85 +11,91 @@ export const EncryptAndUploadFile = async (
   req: Request,
   res: Response
 ): Promise<Response> => {
-  const file = req.files?.file as fileUpload.UploadedFile;
-  var chunkSize: number = Math.floor(file.size / 16);
+  try {
+    const file = req.files?.file as fileUpload.UploadedFile;
+    var chunkSize: number = Math.floor(file.size / 16);
 
-  const buff = fs.readFileSync(file.tempFilePath);
-  const md5Hash = crypto.createHash("md5").update(buff).digest("hex");
+    const buff = fs.readFileSync(file.tempFilePath);
+    const md5Hash = crypto.createHash("md5").update(buff).digest("hex");
 
-  if (chunkSize * 16 < file.size) {
-    chunkSize++;
-  }
+    if (chunkSize * 16 < file.size) {
+      chunkSize++;
+    }
 
-  const decryptedMasterKey = crypto.privateDecrypt(
-    {
-      key: req.user.rsaPrivateKey,
-      padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
-      oaepHash: "sha256",
-    },
-    req.user.encryptedMasterKey
-  );
-
-  const iv = "12345678901234567890123456789012";
-
-  const keyHash: string = crypto
-    .createHash("sha512")
-    .update(decryptedMasterKey)
-    .digest("hex");
-
-  const ivHash: string = crypto.createHash("sha512").update(iv).digest("hex");
-
-  const readStream = fs.createReadStream(file.tempFilePath, {
-    highWaterMark: chunkSize,
-  });
-
-  var encryptedData: Buffer[] = [],
-    start = 0;
-
-  readStream.on("data", (chunk: Buffer) => {
-    const aesKey: Buffer = crypto.scryptSync(
-      keyHash.substring(start * 8, start * 8 + 8),
-      "This is a salt",
-      32
-    );
-
-    const aesIv: Buffer = crypto.scryptSync(
-      ivHash.substring(start * 8, start * 8 + 8),
-      "This is a salt",
-      16
-    );
-
-    encryptedData.push(aesEncrpytion.encryptWithAES(chunk, aesKey, aesIv));
-    start++;
-  });
-
-  readStream.on("end", async () => {
-    const encryptedFile = Buffer.concat(encryptedData);
-
-    const fileUrl = await AWS.uploadToS3(
-      encryptedFile,
-      `${req.user.userName}/${crypto.randomUUID()}`
-    );
-
-    await userModel.findOneAndUpdate(
+    const decryptedMasterKey = crypto.privateDecrypt(
       {
-        _id: req.user._id,
+        key: req.user.rsaPrivateKey,
+        padding: crypto.constants.RSA_PKCS1_OAEP_PADDING,
+        oaepHash: "sha256",
       },
-      {
-        $push: {
-          files: {
-            fileName: file.name,
-            fileUrl: fileUrl,
-            md5Hash: md5Hash,
-          },
-        },
-      }
+      req.user.encryptedMasterKey
     );
-  });
 
-  return res.status(201).json({
-    message: "File uploaded successfully",
-  });
+    const iv = "12345678901234567890123456789012";
+
+    const keyHash: string = crypto
+      .createHash("sha512")
+      .update(decryptedMasterKey)
+      .digest("hex");
+
+    const ivHash: string = crypto.createHash("sha512").update(iv).digest("hex");
+
+    const readStream = fs.createReadStream(file.tempFilePath, {
+      highWaterMark: chunkSize,
+    });
+
+    var encryptedData: Buffer[] = [],
+      start = 0;
+
+    readStream.on("data", (chunk: Buffer) => {
+      const aesKey: Buffer = crypto.scryptSync(
+        keyHash.substring(start * 8, start * 8 + 8),
+        "This is a salt",
+        32
+      );
+
+      const aesIv: Buffer = crypto.scryptSync(
+        ivHash.substring(start * 8, start * 8 + 8),
+        "This is a salt",
+        16
+      );
+
+      encryptedData.push(aesEncrpytion.encryptWithAES(chunk, aesKey, aesIv));
+      start++;
+    });
+
+    readStream.on("end", async () => {
+      const encryptedFile = Buffer.concat(encryptedData);
+
+      const fileUrl = await AWS.uploadToS3(
+        encryptedFile,
+        `${req.user.userName}/${crypto.randomUUID()}`
+      );
+
+      await userModel.findOneAndUpdate(
+        {
+          _id: req.user._id,
+        },
+        {
+          $push: {
+            files: {
+              fileName: file.name,
+              fileUrl: fileUrl,
+              md5Hash: md5Hash,
+            },
+          },
+        }
+      );
+    });
+
+    return res.status(201).json({
+      message: "File uploaded successfully",
+    });
+  } catch (error) {
+    console.log(error);
+
+    return res.status(500).json({ message: "Something went wrong" });
+  }
 };
 
 // var newData: Buffer[] = [];
--- src/middlewares/auth.ts
+++ src/middlewares/auth.ts
@@ -34,6 +34,8 @@ export const verify = async (
       });
     }
 
+    console.log(error);
+
     return res.status(500).json({ message: "Something went wrong" });
   }
 };
